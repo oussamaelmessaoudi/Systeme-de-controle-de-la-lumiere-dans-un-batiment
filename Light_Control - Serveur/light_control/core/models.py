18810ddaf3639666cc305f38d9c9ca1e7abb70c1
unknown <oussamaelmessaoudi17@gmail.com>
2025-05-04 21:42:46 +0100

    Fresh commit

---
+++ Light_Control - Serveur/light_control/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Zone(models.Model):
+
+class Programmation(models.Model):
+
+class Permission(models.Model):
+
+class LogActivite(models.Model):
